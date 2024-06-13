4668c8d97735591c28dd8fb61ff930b268af5852
josevsousa <jose.vicente.de.sousa@gmail.com>
2024-06-13 10:36:52 -0300

    refatorando a section experience ok

--- src/app/components/card-experience/card-experience.component.ts
+++ src/app/components/card-experience/card-experience.component.ts
@@ -4,19 +4,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'card-experience',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './card-experience.component.html',
   styleUrl: './card-experience.component.scss'
 })
 export class CardExperienceComponent {
 
   @Input() card!: any;
-
-  dataCards: any = { 
-    id: 'portifolio',
-    project: "José Vicente de Sousa",
-    contribution: " Developer  FullStack",
-    tecnologic: "Python, Web2py "
-  }
 
 }
