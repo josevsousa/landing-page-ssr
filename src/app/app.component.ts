a0d7c19f03acb789f9aeca83ad6c930de8d0be51
josevsousa <jose.vicente.de.sousa@gmail.com>
2024-07-01 06:57:53 -0300

    retirando o router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SectionAboutComponent } from './sections/home-sections/section-about/section-about.component';
+import { SectionMainComponent } from './sections/home-sections/section-main/section-main.component';
+import { SectionEducationComponent } from './sections/home-sections/section-education/section-education.component';
+import { SectionExperienceComponent } from './sections/home-sections/section-experience/section-experience.component';
+import { SectionSkillsComponent } from './sections/home-sections/section-skills/section-skills.component';
+import { SectionContatoComponent } from './sections/home-sections/section-contato/section-contato.component';
+import { SectionFooterComponent } from './sections/home-sections/section-footer/section-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    SectionMainComponent,
+    SectionAboutComponent,
+    SectionEducationComponent,
+    SectionExperienceComponent,
+    SectionSkillsComponent,
+    SectionContatoComponent,
+    SectionFooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
